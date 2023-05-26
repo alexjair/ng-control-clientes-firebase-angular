f344d307f15315890dfc8b4fb386d6bf5fcc4dd8
Alexander Jair <alexjair@gmail.com>
2023-05-25 23:32:31 -0500

    Subir app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TableroComponent},
+  {path: 'registrarse', component: RegistroComponent},
+  {path: 'configuracion', component: ConfiguracionComponent},
+  {path: 'cliente/editar/:id', component: EditarClienteComponent},
+
+  //final
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: NoEncontradoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore:{
+    apiKey: "AIzaSyAc16dnPnmie1zYxcAJjONoIBWw9TCkERs",
+    authDomain: "control-clientes-84227.firebaseapp.com",
+    projectId: "control-clientes-84227",
+    storageBucket: "control-clientes-84227.appspot.com",
+    messagingSenderId: "1083589543092",
+    appId: "1:1083589543092:web:9a0cbeafbf116438d403e1",
+    measurementId: "G-B2749H9S7K"
+  }
 };
